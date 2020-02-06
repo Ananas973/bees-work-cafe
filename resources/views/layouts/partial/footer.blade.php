2590ecb4dc3da51abd1a17788425de77357e39d0
Ananas973 <anaise.constant@gmail.com>
2020-02-06 20:16:39 -0300

    1er commit

---
+++ resources/views/layouts/partial/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <p class="copyright pull-right">
+            &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>
+            <strong> Développé &amp; <i class="far fa-heart"></i> par </strong>
+                        <a href="https://www.youtube.com/channel/UCwbVAlvrsD2Tpx93bleNbOA" target="_blank">Simplon Code Girls</a>
+        </p>
+    </div>
+</footer>
